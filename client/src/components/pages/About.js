22789df089b7e6af4a79b3fbbc400055a9861f66
ahsanreal4 <44998349+ahsanreal4@users.noreply.github.com>
2021-06-26 14:27:17 +0500

    Add files via upload

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-primary">About this App</h1>
+      <p className="my-1">
+        This is a full stack react app for storing contacts
+      </p>
+      <p className="my-1">
+        Made By: <strong>Ahsan Azeem</strong>
+      </p>
+      <p className="bg-dark p">
+        <strong>Version: </strong>1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
